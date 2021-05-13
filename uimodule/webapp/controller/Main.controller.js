960aaafdb2dbbe2d4220a2d451af2141ce1df025
Balwant Singh <singh.balwant1104@gmail.com>
2021-05-13 19:09:14 +0530

    initial commit

---
+++ uimodule/webapp/controller/Main.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "com/candent/tools/taskManager/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("com.candent.tools.taskManager.controller.Main", {});
+});
