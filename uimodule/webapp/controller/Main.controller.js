df94e7899cbd7f7743f2422b67647a2b2b69b3a4
Balwant Singh <singh.balwant1104@gmail.com>
2021-05-13 19:33:37 +0530

    initial structure

--- uimodule/webapp/controller/Main.controller.js
+++ uimodule/webapp/controller/Main.controller.js
@@ -1,7 +1,18 @@
 sap.ui.define([
   "com/candent/tools/taskManager/controller/BaseController"
-], function(Controller) {
+], function (Controller) {
   "use strict";
 
-  return Controller.extend("com.candent.tools.taskManager.controller.Main", {});
+  return Controller.extend("com.candent.tools.taskManager.controller.Main", {
+    onInit: function () {
+      if (sap.ui.Device.system.desktop) {
+        this.getView().addStyleClass("sapUiSizeCompact");
+      }
+    },
+    onCollapseExpandPress: function () {
+      var oSideNavigation = this.byId("sideNavigation");
+      var bExpanded = oSideNavigation.getExpanded();
+      oSideNavigation.setExpanded(!bExpanded);
+    }
+  });
 });
